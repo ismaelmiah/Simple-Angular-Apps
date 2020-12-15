96295389b540a48a472f543861a62b28f4ae0171
ismail5g <ismail27.dec@gmail.com>
2020-12-15 23:22:11 +0600

    Basic Complete

---
+++ hello-world/src/app/Server/Server.component.ts
@@ -0,0 +1,17 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './Server.component.html',
+    styles: [`
+    
+h1{
+    padding: 50px;
+    color: yellowgreen;
+}
+    `]
+})
+export class ServerComponent{
+
+}
--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './Server/Server.component';
+import { ServersComponent } from './servers/servers.component';
+import { TestingComponent } from './testing/testing.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { userInputComponent } from './user-input/userInput';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    TestingComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    userInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
