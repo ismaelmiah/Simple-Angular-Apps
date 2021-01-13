90e82dea98a9aae11fb0fa785ff3118732beb092
ismail5g <ismail27.dec@gmail.com>
2021-01-13 10:26:46 +0600

    Apply Subject Observable

--- hello-world/src/app/recipes/recipe.service.ts
+++ hello-world/src/app/recipes/recipe.service.ts
@@ -1,36 +1,27 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list-service';
 import { Recipe } from './recipe.model';
 
-
 @Injectable()
-
 export class RecipeService {
-  constructor(private shoppingListService: ShoppingListService){}
-  recipeSelected = new EventEmitter<Recipe>();
-  
+  constructor(private shoppingListService: ShoppingListService) {}
+
   private recipes: Recipe[] = [
     new Recipe(
       1,
       'A test Recipe',
       'This is simply a test',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20),
-      ]
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       2,
       'A very Recipe',
       'This is simply a test',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Burger', 20)
-      ]
-    )
+      [new Ingredient('Buns', 2), new Ingredient('Burger', 20)]
+    ),
   ];
 
   getRecipes() {
@@ -38,15 +29,13 @@ export class RecipeService {
   }
 
   getRecipe(id: number) {
-    const recipe = this.recipes.find(
-      (s) => {
-        return s.id === id;
-      }
-    );
+    const recipe = this.recipes.find((s) => {
+      return s.id === id;
+    });
     return recipe;
   }
 
-  addIngredientsToShoppingList(ingredient: Ingredient[]){
+  addIngredientsToShoppingList(ingredient: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredient);
   }
 }
--- hello-world/src/app/shopping-list/shopping-list.component.ts
+++ hello-world/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from './shopping-list-service';
 
@@ -7,20 +8,21 @@ import { ShoppingListService } from './shopping-list-service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private igChangeSub: Subscription;
+
   constructor(private shoppingListService: ShoppingListService) { }
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
+    this.igChangeSub = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredient: Ingredient[]) => {
         this.ingredients = ingredient;
       }
     )
-  }
-
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
   }
 }
