6ce29911ce3412abb9cb34e2ab3176576e11b012
ismail5g <ismail27.dec@gmail.com>
2021-01-14 18:51:43 +0600

    Recipe Added/Update Successfully

--- hello-world/src/app/recipes/recipe.model.ts
+++ hello-world/src/app/recipes/recipe.model.ts
@@ -1,20 +1,17 @@
 import { Ingredient } from '../shared/Ingredient.model';
 
 export class Recipe {
-  public id: number;
   public name: string;
   public description: string;
   public imagePath: string;
   public ingredients: Ingredient[];
 
   constructor(
-    id: number,
     name: string,
     desc: string,
     imagepath: string,
     ingredient: Ingredient[]
   ) {
-    this.id = id;
     this.name = name;
     this.description = desc;
     this.imagePath = imagepath;
--- hello-world/src/app/recipes/recipe.service.ts
+++ hello-world/src/app/recipes/recipe.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list-service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+
+  recipesChanged = new Subject<Recipe[]>();
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   private recipes: Recipe[] = [
     new Recipe(
-      1,
       'A test Recipe',
       'This is simply a test',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg',
       [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      2,
       'A very Recipe',
       'This is simply a test',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg',
@@ -29,13 +31,20 @@ export class RecipeService {
   }
 
   getRecipe(id: number) {
-    const recipe = this.recipes.find((s) => {
-      return s.id === id;
-    });
-    return recipe;
+    return this.recipes[id];
   }
 
   addIngredientsToShoppingList(ingredient: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredient);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
